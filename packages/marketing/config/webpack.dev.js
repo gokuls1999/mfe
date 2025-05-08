1c9acc201c4edea5d4d674002596842bfeb7fc12
Gokulakrishnan Subramaniyan <gsubramaniyan@msystechnologies.com>
2025-05-08 10:39:24 +0530

    init

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,29 @@
+const {merge} = require("webpack-merge")
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const commonConfig = require("./webpack.common")
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
+const pacakgeJson = require("../package.json")
+const devConfig = {
+    mode: "development",
+    devServer : {
+        port : 8081,
+        historyApiFallback:{
+            index : 'index.html'
+        }
+    },
+    plugins : [
+        new ModuleFederationPlugin({
+            name:"marketing",
+            filename: "remoteEntry.js",
+            exposes:{
+                "./MarketingApp" : "./src/bootstrap"
+            },
+            shared: pacakgeJson.dependencies
+        }),
+        new HtmlWebPackPlugin({
+            template : './public/index.html'
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig);
