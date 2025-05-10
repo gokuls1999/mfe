d0473f8032ffd7eb0de6db4bb1289c5dc4d935f1
Gokulakrishnan Subramaniyan <gsubramaniyan@msystechnologies.com>
2025-05-10 08:53:20 +0530

    pipeline

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const {merge} = require("webpack-merge")
-const HtmlWebPackPlugin = require('html-webpack-plugin')
 const commonConfig = require("./webpack.common")
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
 const packageJson = require("../package.json")
@@ -20,9 +19,6 @@ const devConfig = {
             },
             shared: packageJson.dependencies
         }),
-        new HtmlWebPackPlugin({
-            template : './public/index.html'
-        })
     ]
 }
 
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const {merge} = require("webpack-merge")
-const HtmlWebPackPlugin = require('html-webpack-plugin')
 const commonConfig = require("./webpack.common")
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
 const pacakgeJson = require("../package.json")
@@ -20,9 +19,7 @@ const devConfig = {
             },
             shared: pacakgeJson.dependencies
         }),
-        new HtmlWebPackPlugin({
-            template : './public/index.html'
-        })
+        
     ]
 }
 
